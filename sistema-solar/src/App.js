e676bae09a9e41054f87537997268e95c721323f
JasonJordanP <jordanperezurrutia@gmail.com>
2022-11-02 03:19:39 -0300

    Pagina del sistema solar lista

--- sistema-solar/src/App.js
+++ sistema-solar/src/App.js
@@ -49,6 +49,9 @@ function App() {
           imagen = 'neptuno'
           descripcion = 'Neptuno es el octavo planeta en distancia respecto al Sol y el más lejano del sistema solar. Forma parte de los denominados planetas exteriores, y dentro de estos, es uno de los gigantes helados, y es el primero que fue descubierto gracias a predicciones matemáticas. Su nombre fue puesto en honor al dios romano del mar —Neptuno—, y es el cuarto planeta en diámetro y el tercero más grande en masa. Su masa es diecisiete veces la de la Tierra.'
         />
+      <div className = 'video'>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/8dy27u_kX1I?start=358" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
       </div>
     </div>
   );
